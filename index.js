20189c5b35661327cf92354776e71ae6478e0323
ahshasib <154332574+ahshasib@users.noreply.github.com>
2026-03-20 02:50:29 +0600

    added login and register api

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose"; 
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 dotenv.config();
 
@@ -15,6 +17,94 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected ✅"))
   .catch((err) => console.log(err));
+
+
+// login and registration api start from here
+
+// Schema
+const userSchema = new mongoose.Schema({
+  phone: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  invitationCode: String,
+}, { timestamps: true });
+
+const User = mongoose.model("User", userSchema);
+
+// REGISTER
+app.post("/api/register", async (req, res) => {
+  try {
+    const { phone, password, confirmPassword, invitationCode } = req.body;
+
+    if (!phone || !password) {
+      return res.status(400).json({ message: "সব field পূরণ করুন" });
+    }
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ message: "Password not match" });
+    }
+
+    const exist = await User.findOne({ phone });
+
+    if (exist) {
+      return res.status(400).json({ message: "এই নাম্বারে আগেই account আছে" });
+    }
+
+    const hash = await bcrypt.hash(password, 10);
+
+    await User.create({
+      phone,
+      password: hash,
+      invitationCode,
+    });
+
+    res.json({ message: "Register success ✅" });
+
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+
+// LOGIN
+app.post("/api/login", async (req, res) => {
+  try {
+    const { phone, password } = req.body;
+
+    const user = await User.findOne({ phone });
+
+    // ❌ user নাই → register করতে বলবে
+    if (!user) {
+      return res.status(400).json({
+        message: "Account নেই, আগে Register করুন ❗"
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    // ❌ password ভুল
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Wrong password ❌"
+      });
+    }
+
+    const token = jwt.sign(
+      { id: user._id },
+      "secret123",
+      { expiresIn: "7d" }
+    );
+
+    res.json({
+      message: "Login success ✅",
+      token,
+      user
+    });
+
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 
 // 🔹 Withdrawal Schema + Model
 const withdrawalSchema = new mongoose.Schema({
